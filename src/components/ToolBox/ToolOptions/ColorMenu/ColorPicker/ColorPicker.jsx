c96b4a05b3d98608aa1bf62a18b507aaf3487e4d
unknown <pierrick@live.co.uk>
2022-01-26 17:01:15 +0000

    Picker tool display stuff

--- src/components/ToolBox/ToolOptions/ColorMenu/ColorPicker/ColorPicker.jsx
+++ src/components/ToolBox/ToolOptions/ColorMenu/ColorPicker/ColorPicker.jsx
@@ -1,16 +1,22 @@
-import { BsEyedropper } from "react-icons/bs";
-import { useBrushColor } from "../../../../../hooks/useBrushColor";
+import { BsEyedropper } from "react-icons/bs"
+import { useBrushColor } from "../../../../../hooks/useBrushColor"
+import {colorToCanvasColor} from "../../../../../core/utils/colors"
+
 
 import "./ColorPicker.css"
 
 export default function ColorPicker() {
     const {brushColor, setBrushColor} = useBrushColor()
 
+    const useColorPicker = function() {
+
+    }
+
     const render = function() {
         return (
             <div className="ColorPicker">
-                <button className="colorPickerButton"><BsEyedropper/></button>
-                <div></div>
+                <button className="colorPickerButton" onClick={useColorPicker}><BsEyedropper/></button>
+                <div className="colorPickerDisplay" style={{background: colorToCanvasColor(brushColor)}}/>
             </div>
         )
     }
