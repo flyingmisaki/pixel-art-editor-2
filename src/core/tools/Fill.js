3e048d3bb35a08b215cd0724d11ca8574f4d268e
unknown <pierrick@live.co.uk>
2022-01-31 15:40:26 +0000

    Started work on flood fill tool

--- src/core/tools/Fill.js
+++ src/core/tools/Fill.js
@@ -1,15 +1,72 @@
+import {colorToCanvasColor} from "../utils/colors"
 import {BsPaintBucket} from "react-icons/bs"
 
 class Fill {
     constructor() {
         this.name = "Fill"
+        this.usesColors = true
+        this.status = ""
+        
+        this.previewCanvasContext = null
+        this.canvasContext = null
+        this.options = {
+            scale : 1,
+            AA : false,
+            replace : false
+        }
+        this.drawing = false
     }
+
+    updateStatus(status) {
+        this.status = status
+        this.onStatusChange(status)
+    }
+
+    onStatusChange(status) {}
 
     renderIcon() {
         return <BsPaintBucket/>
     }
 
-    mouseDown(context, x, y, color) {
+    floodFill(position, color) {
+        
+    }
+
+    getColor(position) {
+        const pixelData = this.canvasContext.getImageData(position.x, position.y, 1, 1)
+        const [r, g, b, a] = pixelData.data
+        return {
+            r,
+            g,
+            b,
+            a: a/255
+        }
+    }
+
+    drawPixel(context, position, color) {
+        if (!context) return
+        context.fillStyle = colorToCanvasColor(color)
+        // Clears before placing color again so it doesn't add up with transparency ect...
+        context.fillRect(position.x, position.y, this.options.scale, this.options.scale)
+    }
+
+    mouseDown(position, color) {
+        this.position = position
+
+        this.floodFill(position, color)
+
+        // this.drawing = true
+        // if (this.drawing) this.updateStatus(`Drawing pixel at ${position.x}, ${position.y}`)
+    }
+
+    mouseUp(position, color) {
+        // this.drawing = false
+    }
+
+    mouseMove(position, color) {
+        // if (this.drawing) this.updateStatus(`Drawing pixel at ${position.x}, ${position.y}`)
+        // const context = this.drawing ? this.canvasContext : this.previewCanvasContext
+        // this.drawPixel(context, position, color)
     }
 }
 
