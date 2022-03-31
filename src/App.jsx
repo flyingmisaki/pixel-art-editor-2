c9edbb5d67872aad29d120e2863eb7494aeb6b30
unknown <pierrick@live.co.uk>
2022-03-31 15:46:58 +0100

    Fixed title

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
     	return (
       		<div className="App">
 				<Helmet>
-					<title>pixel</title>
+					<title>Pixel</title>
 					<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 				</Helmet>
 
